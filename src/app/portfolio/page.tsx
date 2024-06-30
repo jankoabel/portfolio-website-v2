24203b79714b2427e59539720ea624cf3305320a
jankoabel <111200082+jankoabel@users.noreply.github.com>
2024-06-30 11:11:06 -0700

    Update page.tsx

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -5,7 +5,7 @@ const Portfolio: NextPage = () => {
   const portfolioItems = [
     {
       id: 1,
-      image: "/imgs/portfolio/ecommerce.PNG", // Placeholder image URL
+      image: "/imgs/portfolio/ecommerce.PNG",
       title: "Landing Page for Ecommerce Website",
       description: "A beautiful user interface(UI) for an Ecommerce Website.",
       techStacks: ["React", "MUI", "antd"],
@@ -14,7 +14,7 @@ const Portfolio: NextPage = () => {
     },
     {
       id: 2,
-      image: "/imgs/portfolio/dictionary.PNG", // Placeholder image URL
+      image: "/imgs/portfolio/dictionary.PNG",
       title: "English Dictionary",
       description: "A Website where users can search and get words and get the meaning, synonym, antonym, pronunciation and even images for that word.",
       techStacks: ["React", "tailwindcss", "react icons", "RapidApi"],
@@ -23,25 +23,16 @@ const Portfolio: NextPage = () => {
     },
     {
       id: 3,
-      image: "/imgs/portfolio/education.PNG", // Placeholder image URL
+      image: "/imgs/portfolio/education.PNG",
       title: "Landing Page for Educational Website",
       description: "A beautiful user interface(UI) for Educational Website.",
       techStacks: ["HTML", "CSS", "JS"],
       githubLink: "", // No GitHub link provided
       demoLink: "https://educationallandingpage.netlify.app",
     },
-    // {
-    //   id: 4,
-    //   image: "/imgs/portfolio/tours.PNG", // Placeholder image URL
-    //   title: "Tour and Travel Website",
-    //   description: "The Tour and Travel Website is a dynamic online platform built with React, utilizing modern web technologies such as react-icons, antd, axios, and react-helmet.",
-    //   techStacks: ["React", "react icons", "antd", "axios", "react-helmet"],
-    //   githubLink: "https://github.com/jankoabel/enat_ethiopia_tours",
-    //   demoLink: "https://enatethiopiatours.com",
-    // },
     {
       id: 4,
-      image: "https://media.licdn.com/dms/image/C4D33AQEr351RRfbS3w/productpage-image_1128_635/0/1628572482298/oreference_school_management_system_image?e=2147483647&v=beta&t=_te239RqrMa3R002CGiEg6tRFxVyjEfZ9FXSkIlybig", // Placeholder image URL
+      image: "https://media.licdn.com/dms/image/C4D33AQEr351RRfbS3w/productpage-image_1128_635/0/1628572482298/oreference_school_management_system_image?e=2147483647&v=beta&t=_te239RqrMa3R002CGiEg6tRFxVyjEfZ9FXSkIlybig",
       title: "School Management System with ASP.NET Core 7 MVC",
       description: "The School Management System is a comprehensive web application developed using ASP.NET Core 7 MVC. It utilizes Entity Framework Core for database management and SQL Server for data storage.",
       techStacks: ["ASP.NET Core 7 MVC", "Entity Framework Core", "SQL Server", "Bootstrap", "jQuery", "AutoMapper"],
@@ -50,15 +41,59 @@ const Portfolio: NextPage = () => {
     },
     {
       id: 5,
-      image: "/imgs/portfolio/blog.PNG", // Placeholder image URL
+      image: "/imgs/portfolio/blog.PNG",
       title: "FineBlog",
       description: "FineBlog is a web application built using .NET Core MVC and SQL Server. It provides a platform for users to create and manage their own blogs.",
       techStacks: [".Net Core MVC", "SQL SERVER"],
       githubLink: "https://github.com/jankoabel/blog",
       demoLink: "", // No demo link provided
     },
+    {
+      id: 6,
+      image: "https://cdn-www.oktopost.com/blog/wp-content/uploads/2022/03/social-media-sentiment-analysis-blog-cover.png", // No image URL provided
+      title: "Social Media Sentiment Analysis",
+      description: "A project analyzing sentiment on social media platforms using machine learning techniques.",
+      techStacks: ["Python", "NLTK", "scikit-learn"],
+      githubLink: "https://github.com/jankoabel/Social-Media-Sentiment-Analysis",
+      demoLink: "", // No demo link provided
+    },
+    {
+      id: 7,
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlmNQNvAvu778s7BObeCK5waAoyhba2C9JLw&s",
+      title: "Movie Recommendation System using R",
+      description: "A movie recommendation system built using R and various collaborative filtering techniques.",
+      techStacks: ["R", "Recommenderlab"],
+      githubLink: "https://github.com/jankoabel/Movie-Recommendation-System-using-R_Project",
+      demoLink: "", // No demo link provided
+    },
+    {
+      id: 8,
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3OM4fn3HTL-MqbZO7rtRd6VzGqGcPBWRbiQ&s",
+      title: "Spam Email Classification",
+      description: "A machine learning project for classifying emails as spam or not spam.",
+      techStacks: ["Python", "scikit-learn", "Pandas"],
+      githubLink: "https://github.com/jankoabel/Spam-Email-Classification",
+      demoLink: "", // No demo link provided
+    },
+    {
+      id: 9,
+      image: "https://miro.medium.com/v2/resize:fit:372/1*AO2rIhzRYzFVQlFLx9DM9A.png",
+      title: "Deep Learning Handwritten Digit Recognition",
+      description: "A deep learning project for recognizing handwritten digits using a neural network.",
+      techStacks: ["Python", "TensorFlow", "Keras"],
+      githubLink: "https://github.com/jankoabel/Deep-Learning-Handwritten-Digit-Recognition",
+      demoLink: "", // No demo link provided
+    },
+    {
+      id: 10,
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNtqGGLvIl9HBB8t9b_Ayxlx1HrfZcaI28kw&s",
+      title: "Customer Care Bot",
+      description: "A chatbot application for customer care service using NLP techniques.",
+      techStacks: ["Python", "NLTK", "Rasa"],
+      githubLink: "https://github.com/jankoabel/-CustomerCare_Bot",
+      demoLink: "", // No demo link provided
+    },
   ];
-
 
   return (
     <div className="bg-gray-100 dark:bg-gray-800">
